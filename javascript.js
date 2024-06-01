f9ce327e679ce70857d8d934de6d7e85f2c917bb
mdot2x3 <mdot2x3@gmail.com>
2024-05-31 20:22:06 -0700

    feat: Add core calculator functions

---
+++ javascript.js
@@ -0,0 +1,39 @@
+// calculator input functions
+function add(a, b){
+    return a + b;
+};
+
+function subtract(a, b){
+    return a - b;
+};
+
+function multiply(a, b){
+    return a * b;
+};
+
+function divide(a, b){
+    return a / b;
+};
+
+// variables to store user selection
+let firstNumber
+let secondNumber
+let operator
+
+// function to combine input and calc function
+function operate(operator, firstNumber, secondNumber) {
+    switch (operator) {
+        case '+':
+            return add(firstNumber, secondNumber);
+            break;
+        case '-':
+            return subtract(firstNumber, secondNumber);
+            break;
+        case '*':
+            return multiply(firstNumber, secondNumber);
+            break;
+        case '/':
+            return divide(firstNumber, secondNumber);
+            break;
+    }
+}
